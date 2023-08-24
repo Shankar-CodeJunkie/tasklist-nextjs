b8e21c6c198d599ef28a1e8eba356d542142a61f
Shankar-CodeJunkie <emailmeforshankar@gmail.com>
2023-08-24 16:33:16 +0530

    fix: add custom header to prevent verceljs caching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/tasklist',
+                headers: [
+                    {
+                        key:"Cache-Control",
+                        value: "no-store, no-cache, must-revalidate, proxy-revalidate"
+                    }
+                ]
+            },
+            {
+                source: '/api/*',
+                headers: [
+                    {
+                        key:"Cache-Control",
+                        value: "no-store, no-cache, must-revalidate, proxy-revalidate"
+                    }
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
